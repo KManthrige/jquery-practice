309f74617f8bfd844158f9bc4c9988859f855944
kmanthrige <k.manthrige@gmail.com>
2023-05-08 11:39:13 -0500

    added two functions to display budget and add purchases

--- jquery.js
+++ jquery.js
@@ -1,7 +1,23 @@
-console.log('js')
-
 $(document).ready(readyNow)
 
-function readyNow(){
-    console.log('jQuery')
+const budget = 25000;
+let purchases =[];
+
+function addPurchase(){
+    let newPurchase = {
+        name: $('#purchaseNameIn').val(),
+        price: $('#purchasePriceIn').val()
+    }
+    purchases.push(newPurchase)
+    //clear the input boxes
+    $('#purchaseNameIn').val('')
+    $('#purchasePriceIn').val('')
 }
+
+function readyNow(){
+    let sb = $('#startingBudget')
+    sb.append (budget)
+    //display the const budget on the dom
+    $('#addPurchaseButton').on('click', addPurchase)
+} 
+
