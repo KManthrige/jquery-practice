f3c10fd5762b9ed03a18d8496cff0dbbf1fc29a4
kmanthrige <k.manthrige@gmail.com>
2023-05-08 16:33:33 -0500

    completed the budget app

--- jquery.js
+++ jquery.js
@@ -1,9 +1,9 @@
 $(document).ready(readyNow)
 
 const budget = 25000;
-let purchases =[];
+let purchases = [];
 
-function addPurchase(){
+function addPurchase() {
     let newPurchase = {
         name: $('#purchaseNameIn').val(),
         price: $('#purchasePriceIn').val()
@@ -12,12 +12,38 @@ function addPurchase(){
     //clear the input boxes
     $('#purchaseNameIn').val('')
     $('#purchasePriceIn').val('')
+    displayPurchases()
+    caluculateBalance()
 }
 
-function readyNow(){
+function caluculateBalance() {
+    let totalCost = 0
+    let remainder = 0
+    for (i = 0; i < purchases.length; i++){
+        totalCost += Number(purchases[i].price)
+        console.log('total cost', totalCost)
+    }
+    remainder = budget-totalCost
+    console.log('balance', remainder)
+    let el = $('#remainingBudget')
+    el.empty()
+    el.append(remainder)
+}
+
+function displayPurchases() {
+    let pm = $('#purchasesMade')
+    pm.empty()
+    for (i = 0; i < purchases.length; i++) {
+        pm.append(`<li>` + purchases[i].name + ': $' + purchases[i].price + `</li>`)
+    }
+}
+
+
+function readyNow() {
     let sb = $('#startingBudget')
-    sb.append (budget)
+    sb.append(budget)
     //display the const budget on the dom
     $('#addPurchaseButton').on('click', addPurchase)
-} 
+
+}
 
